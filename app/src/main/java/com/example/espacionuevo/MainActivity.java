058e45567402be08b807980de37415a688422860
augustorojas2019 <augusto.rojas@tisi.com.pe>
2021-05-05 21:23:20 -0500

    Segunda actualizacion

--- app/src/main/java/com/example/espacionuevo/MainActivity.java
+++ app/src/main/java/com/example/espacionuevo/MainActivity.java
@@ -27,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        //  PROBANDO LAS ACTUALIZACIONES
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
